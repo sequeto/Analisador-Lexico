ffe4122141c55359a0ce16c326b170974bb874c9
sequeto <joaopedrosequeto@gmail.com>
2022-07-30 17:01:19 -0300

    Criando Classe para Expressão Regular

---
+++ src/main/java/trabalhodcc196/analisador/model/Regex.java
@@ -0,0 +1,29 @@
+package trabalhodcc196.analisador.model;
+
+
+public class Regex {
+	private String expression;
+
+	public Regex(String regex) {
+		super();
+		this.expression = regex;
+	}
+
+	public String getRegex() {
+		return expression;
+	}
+
+	public void setRegex(String regex) {
+		this.expression = regex;
+	}
+	
+	public AFN processingConcatenation(AFN afnInicial, AFN afnFinal) {
+		AFN afn = new AFN();
+		
+		
+	}
+	
+	
+	public AFN regex
+	
+}
